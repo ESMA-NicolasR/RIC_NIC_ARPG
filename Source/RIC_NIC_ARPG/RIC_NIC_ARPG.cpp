280e7cb9c929ab60fcda6a43227c617b3ee8c154
ESMA-NicolasR <n.richard@rns.ecolescreatives.com>
2025-11-21 12:05:37 +0100

    [ADD] custom CPP class for SPELLS

---
+++ Source/RIC_NIC_ARPG/RIC_NIC_ARPG.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RIC_NIC_ARPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RIC_NIC_ARPG, "RIC_NIC_ARPG" );
